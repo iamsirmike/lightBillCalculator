10831f59bf6dbe669e0e8ed0e967890af2305a10
MICHAEL ASAMOAH <you@example.com>
2019-04-05 16:06:04 +0000

    added if statements

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,25 @@
 function calc(){
    var name = document.getElementsByTagName('input').length;
-   alert(name);
+   if (name === 2) {
+      alert("use id 1")
+   } else if (name === 4) {
+      alert("use id 2")
+   } else if (name === 6) {
+      alert("use id 3")
+   } else if (name === 8) {
+      alert("use id 4")
+   } else if (name === 10) {
+      alert("use id 5")
+   } else if (name === 12) {
+      alert("use id 6")
+   } else if (name === 14) {
+      alert("use id 7")
+   } else if (name === 16) {
+      alert("use id 8")
+   } else if (name === 18) {
+      alert("use id 9")
+   } else if (name === 20) {
+      alert("use id 10")
+   } 
 
 }
